1534ef39ecf4d67d153fe634beec7f9b799f54c4
HARSH RAO <quasarhrsh@gmail.com>
2023-11-24 00:08:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Install the dependencies using requirements.txt
+
+```bash
+pip install -r requirements.txt
+```
+
+
+Run the App
+```bash
+streamlit run app.py
+```
---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://www.transparentpng.com/thumb/user/blue-male-user-profile-transparent-png-2lbgMx.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://www.pngfind.com/pngs/m/16-168087_wikipedia-user-icon-bynightsight-user-image-icon-png.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
